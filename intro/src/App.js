9d75c17ffab625ae473c64443742037a9b511420
esince <esinece6@gmail.com>
2020-04-05 19:22:40 +0300

    Parsing error in App js hierarchy

--- intro/src/App.js
+++ intro/src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div>
       <h2>Hello from react </h2>
     </div>
+    //  Parsing error: Adjacent JSX elements must be 
+    // wrapped in an enclosing tag. Did you want a JSX fragment <>
+    // <h3></h3>
   );
 }
 
