667047ad147219449cf2da97f46bdacc86e9e053
esince <esinece6@gmail.com>
2020-04-10 19:19:02 +0300

    components imported to app.js

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Navi from './Navi'
+import CategoryList from './CategoryList';
+import ProductList from './ProductList';
 
 
 function App() {
   return (
     <div>
-      <h2>Hello from react </h2>
-    </div>
+      <h2>Hello from Ayşe Nur </h2>
+      <h1>
+        Hello from Mehmet Kaan
+        </h1>  
+        <Navi/>
+        <CategoryList/>
+        <ProductList/>
+      </div>
     //  Parsing error: Adjacent JSX elements must be 
     // wrapped in an enclosing tag. Did you want a JSX fragment <>
     // <h3></h3>
