30d0c6c8a75cc65ca0eba35bc26a0de52b849f7b
esince <esinece6@gmail.com>
2020-04-16 14:24:38 +0300

    simple example for this.state = {}

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -3,14 +3,15 @@ import {ListGroup,ListGroupItem } from 'reactstrap'
 
 export default class CategoryList extends Component {
     constructor(props){
-        super(props)
-        state:{}
+        super(props) //component that is extended.
+        this.state = {counter:1}
 
     }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
         <ListGroup>
           <ListGroupItem>Cras justo odio</ListGroupItem>
           <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
