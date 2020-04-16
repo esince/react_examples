5caa075b4d9a1732794c90923fe7ca758def9ac6
esince <esinece6@gmail.com>
2020-04-16 14:34:09 +0300

    state learned. -- in this step a warning arised = "Warning: Each child in a list should have a unique "key" prop."

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -1,23 +1,25 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem } from 'reactstrap'
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-    constructor(props){
-        super(props) //component that is extended.
-        this.state = {counter:1}
-
-    }
+  constructor(props) {
+    super(props); //component that is extended.
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <h3>{this.state.counter}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.categories.map((category) => (
+            <ListGroupItem>{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
       </div>
     );
