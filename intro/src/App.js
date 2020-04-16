0c7a7416a5ed86f4a5de748a58db9fad4ec86a74
esince <esinece6@gmail.com>
2020-04-16 13:39:20 +0300

    reactstrap - {Container, Row, Col} are learned

--- intro/src/App.js
+++ intro/src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import Navi from './Navi'
-import CategoryList from './CategoryList';
-import ProductList from './ProductList';
-
+import React from "react";
+import Navi from "./Navi";
+import CategoryList from "./CategoryList";
+import ProductList from "./ProductList";
+import { Container, Row , Col} from "reactstrap";
 
 function App() {
   return (
     <div>
-      <h2>Hello from Ayşe Nur </h2>
-      <h1>
-        Hello from Mehmet Kaan
-        </h1>  
-        <Navi/>
-        <CategoryList/>
-        <ProductList/>
-      </div>
-    //  Parsing error: Adjacent JSX elements must be 
+      <Container>
+        <Row>
+          <Navi />
+        </Row> 
+        <Row>
+          <Col xs = "3" > <CategoryList /> </Col> 
+          <Col xs = "9" > <ProductList /> </Col> 
+        </Row>
+      </Container>
+
+      
+    </div>
+    //  Parsing error: Adjacent JSX elements must be
     // wrapped in an enclosing tag. Did you want a JSX fragment <>
     // <h3></h3>
   );
